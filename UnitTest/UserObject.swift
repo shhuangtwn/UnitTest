8275a6f051a317e30ccfd092bf97f09fc70baae6
shhuangtwn <shanggoo@gmail.com>
2016-10-17 09:52:37 +0800

    add user object protocol

---
+++ UnitTest/UserObject.swift
@@ -0,0 +1,17 @@
+//
+//  UserObject.swift
+//  UnitTest
+//
+//  Created by Bryan Huang on 2016/10/17.
+//  Copyright © 2016年 shhuang. All rights reserved.
+//
+
+import Foundation
+
+protocol UserObject {
+    
+    var identifier: String { get }
+    
+    var name: String { get }
+    
+}
